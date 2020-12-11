2649ff8cffeb00e6fc6a04e481a542a91ee02037
OzanGenc <ozangenc35@hotmail.com>
2020-12-10 22:57:31 -0800

    Add files via upload

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -26,3 +26,13 @@ else:
     pass
 
 
+
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+
+expander = st.beta_expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
+
+

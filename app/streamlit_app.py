b0a664eaf96a9c6cd90938eea85ff2dc88880df6
OzanGenc <ozangenc35@hotmail.com>
2020-12-10 19:32:39 -0800

    Add files via upload

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -24,8 +24,6 @@ if user_input in cocktails_df['Cocktail Name'].tolist():
 
 elif user_input:
     recommend_cocktail_similarity_to_ingredients(user_input=user_input, cocktails_df=cocktails_df, vectorizer=vectorizer, number_of_recommendations=number_of_recommendations)
-    image = Image.open('./ingredient.jpg')
-    st.image(image, use_column_width=True)
 
 else:
     pass
